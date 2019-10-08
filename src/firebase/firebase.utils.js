9e87ff5ae2ae7b0b1d9ac9afabc7b5daf4573851
Canaan Merchant <canaan.merchant@vexterra.com>
2019-10-08 15:26:41 -0400

    firebase work

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { async } from 'q';
 
 const config = {
     apiKey: "AIzaSyC-aEFFqWQRDAa55g_QReM4E7DKtloVICE",
@@ -13,6 +14,32 @@ const config = {
     measurementId: "G-EF40N6GFXK"
   };
 
+  export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if (!userAuth) return;
+
+   const userRef = firestore.doc(`users/${userAuth.uid}`);
+   const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+      const { displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (error) {
+        console.log('Error creating user', error.message);
+      }
+    }
+    return userRef;
+
+  };
+
+  
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
