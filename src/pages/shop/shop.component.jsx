970e5473d02678d2b2cafffc5ac314c49e1d31ec
Canaan Merchant <canaan.merchant@vexterra.com>
2019-12-03 15:28:18 -0500

    new reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,19 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data.js';
-
 import PreviewCollection from '../../components/preview-collection/preview-collection.component';
 import { identifier } from '@babel/types';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'; 
 
-    render() {
 
-        const {collections} = this.state;
+const ShopPage = ({ collections })  => (
+    
+    <div className='shop-page'>
+        <CollectionsOverview />        
+    </div>
+        );
+    
 
-        return(<div className='shop-page'>
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
-                    <PreviewCollection key={id} {...otherCollectionProps}  />
-                ))
-            }
-        </div>)
-    };
-}
+
 
 export default ShopPage;
