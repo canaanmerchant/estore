915a75b5977e97f1017cfec5a8fa7bb3583ad5b4
Canaan Merchant <42153363+canaanmerchant@users.noreply.github.com>
2019-12-12 15:30:54 -0500

    Update stripe-button.component.jsx
    taking out my key

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
-    const publishableKey = 'pk_test_BPKI9zafxonPryrHyuZBjUEd';
+    const publishableKey = 'Insert your key here';
 
 
 const onToken = token => {
